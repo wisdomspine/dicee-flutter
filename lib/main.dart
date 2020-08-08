e313ed755b40fe7f2efc19de63cb18e1cc6c4a1b
Priest Sabo Ombugadu <sabopriest@gmail.com>
2020-08-08 09:05:59 +0100

    ref: updateDiceNumber

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,64 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: SafeArea(
+          child: DicePage(),
+        ),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 1;
+  int _diceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _updateDiceNumber();
+              },
+              child: Image.asset("images/dice$_leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                _updateDiceNumber();
+              },
+              child: Image.asset("images/dice$_rightDiceNumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _updateDiceNumber() {
+    setState(() {
+      _leftDiceNumber = Random().nextInt(6) + 1;
+      _rightDiceNumber = Random().nextInt(6) + 1;
+    });
   }
 }
